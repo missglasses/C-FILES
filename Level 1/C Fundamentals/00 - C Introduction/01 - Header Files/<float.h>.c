641b31c3e2fa2f481cc4aed359a8c2d0e4b3038a
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-06-28 11:04:57 +0800

    Update <float.h>.c

--- Level 1/C Fundamentals/00 - C Introduction/01 - Header Files/<float.h>.c
+++ Level 1/C Fundamentals/00 - C Introduction/01 - Header Files/<float.h>.c
@@ -1,1 +1,31 @@
 
+#include <stdio.h>
+#include <float.h>
+
+
+//PRINTING FLOATING-POINT LIMITS USING FLOAT HEADER
+int main() {
+  
+  printf("Float radix: %d (for binary)\n\n", FLT_RADIX);
+  
+  //Precision
+  printf("Decimal digits (FLT_DIG): %d\n", FLT_DIG);
+  printf("Decimal digits (DBL_DIG) || min: 6: %d\n", DBL_DIG);
+  printf("Decimal digits (LDBL_DIG) || min: 10: %d\n", LDBL_DIG);
+   printf("Decimal digits (DECIMAL_DIG) || min: 10: %d\n\n", DECIMAL_DIG);
+  
+  printf("Float range: %e-%e\n", FLT_MIN, FLT_MAX);
+  printf("Double range: %e-%e\n", DBL_MIN, DBL_MAX);
+   printf("Long double range: %e-%e\n", LDBL_MIN, LDBL_MAX);
+   
+   
+//   machine epsilon : FLT_EPSILON, DBL_EPSILON, LBDL_EPSILON
+
+    printf("Rounding || Float : %d\n", FLT_ROUNDS);
+  //other: FLT_EVAL_METHOD
+  
+  
+  
+  
+    return 0;
+}
