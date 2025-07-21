735a88e22265d65518bef99ed97cb6eb7e57eb8d
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-07-21 22:53:25 +0800

    Create isDescending.c

---
+++ Level 2/08 - ADT List: Linked List/isDescending.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "myheader.h"
+
+// [ANSWER No. 1] insert your function definition for isDesceding()
+bool isDescending(Node *head){
+    //if node is empty or single node
+    if(head==NULL || head->next==NULL){
+        return true;
+    
+    
+    Node *current=head;
+    if (current->data<current->next->data){
+        return false ; //increase not descending
+    }
+    current = current->next;// traverse
+    }
+    return true; //if all are in descenfing ord
+}
+
+
+
+
+// the main()
+int main() {
+    Node* head = NULL;
+    int n, item;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    // Input and insert nodes into the list
+    for(int i = 0; i < n; ++i) {
+        printf("Enter element %d: ", i+1);
+        scanf("%d", &item);
+        insertNode(&head, item);
+    }
+
+    printf("List: ");
+    printList(head);
+
+    // [ANSWER No. 2] Add function call for isDescending()
+    if(isDescending(head)){
+        printf("The list is in descending order.");
+    } else {
+        printf("The list is not in descending order.");
+    }
+
+
+
+    return 0;
+}
