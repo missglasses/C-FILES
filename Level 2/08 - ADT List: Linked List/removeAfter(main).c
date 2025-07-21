47b663c7c2da43794540fc0ce6516e400d536067
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-07-21 23:48:34 +0800

    Update removeAfter(main).c

--- Level 2/08 - ADT List: Linked List/removeAfter(main).c
+++ Level 2/08 - ADT List: Linked List/removeAfter(main).c
@@ -1,61 +1,66 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include "myheader.h"
-
-// [ANSWER No. 1] insert your function definition for removeAfter()
-NodePtr removeAfter(Node *head, int item){
-    if(head==NULL){
-    return NULL;
+    #include <stdio.h>
+    #include <stdlib.h>
+    #include <stdbool.h>
+    #include "myheader.h"
+    
+    // [ANSWER No. 1] insert your function definition for removeAfter()
+    NodePtr removeAfter(Node *head, int item){
+        if(head==NULL){
+        return NULL;
+        }
+        
+        NodePtr current = head;
+        while(current->next!=NULL && current->data!=item){
+            current = current->next; //tr
+        }
+        
+        if (current==NULL || current->next==NULL){
+            return NULL; //no nodes after
+        }
+        
+        NodePtr removedList = current->next; /*takesn node after current and removed list points to the 
+        node to be deleted
+        Before: [current] -> [node_to_remove] -> [next_node] -> NULL
+        */
+        current->next=NULL; /*cuts off; 
+    Before: [current] -> [node_to_remove] -> [next_node] -> NULL
+    After:  [current] -> NULL*/
+        return removedList;
+        
     }
     
-    NodePtr current = head;
-    while(current->next!=NULL && current->data!=item){
-        current = current->next; //tr
-    }
+    // the main()
+    int main() {
+        Node* head = NULL;
+        int n, item;
     
-    if (current==NULL || current->next==NULL){
-        return NULL; //no nodes after
-    }
+        printf("Enter number of elements: ");
+        scanf("%d", &n);
     
-    NodePtr removedList = current->next;
-    current->next=NULL;
-    return removedList;
+        // Input and insert nodes into the list
+        for(int i = 0; i < n; ++i) {
+            printf("Enter element %d: ", i+1);
+            scanf("%d", &item);
+            insertNode(&head, item);
+        }
     
-}
-
-// the main()
-int main() {
-    Node* head = NULL;
-    int n, item;
-
-    printf("Enter number of elements: ");
-    scanf("%d", &n);
-
-    // Input and insert nodes into the list
-    for(int i = 0; i < n; ++i) {
-        printf("Enter element %d: ", i+1);
-        scanf("%d", &item);
-        insertNode(&head, item);
+        printf("List: ");
+        printList(head);
+    
+        int ditem;
+        Node* removedList = NULL;  //Nodeptr -> node
+    
+        printf("\nEnter item: ");
+        scanf("%d", &ditem);
+    
+        //call func
+        removedList = removeAfter(head, ditem);  
+                                                
+    
+        printf("Orginal List (after delete): ");  
+        printList(head);
+        printf("Deleted List: ");
+        printList(removedList);
+    
+        return 0;
     }
-
-    printf("List: ");
-    printList(head);
-
-    int ditem;
-    Node* removedList = NULL;  //Nodeptr -> node
-
-    printf("\nEnter item: ");
-    scanf("%d", &ditem);
-
-    //call func
-    removedList = removeAfter(head, ditem);  
-                                            
-
-    printf("Orginal List (after delete): ");  
-    printList(head);
-    printf("Deleted List: ");
-    printList(removedList);
-
-    return 0;
-}
