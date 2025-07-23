7d19dcee6add24d1226080d15280ea1dc33364a5
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-07-23 09:04:54 +0800

    Update addFirst.c

--- Level 2/08 - ADT List: Linked List/addFirst.c
+++ Level 2/08 - ADT List: Linked List/addFirst.c
@@ -1,1 +1,13 @@
-
+void addFirst(Node **head, int data){
+    Node *newNode = malloc(sizeof(Node));
+    
+    if (head==NULL){
+        printf("Mem alloc failed");
+        exit(1);
+    }
+    
+    newNode->data=data;
+    newNode->next = *head; //points to head
+    
+    *head = newNode; //now head points to newNode
+}
