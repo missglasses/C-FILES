2861cafa0fefc52382439de2ff662fecbd374f2d
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-07-30 13:41:57 +0800

    Create initCB_v1.c

---
+++ Level 3/00 - ADT List/iii. Cursor-based Implementation/initCB_v1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#define MAX 50
+typedef struct
+{
+    char data;
+    int link;
+} NodeType;
+
+typedef struct
+{
+    NodeType Nodes[MAX];
+    int avail;
+} VirtualHeap;
+
+typedef int List;
+
+int main()
+{
+    VirtualHeap VH;
+
+    initVH(&VH);
+    return 0;
+}
+
+void initVirtualHeap(VirtualHeap *vh) {
+    int i;
+
+    // set first available index to 0
+    vh->avail = 0;
+
+    // loop through all nodes except the last 
+    for (i = 0; i < MAX - 1; i++) {
+        // link each node to the next one in the array
+        //nodes: 0 -> 1 -> 2 -> ... -> max-1
+        vh->Nodes[i].link = i + 1;
+    }
+
+    //sentinel to indicate last node's link
+    vh->Nodes[MAX - 1].link = -1;
+}
