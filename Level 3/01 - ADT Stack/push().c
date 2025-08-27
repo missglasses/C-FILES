6bc228c4d91c57816127d30cdf52216c8283f906
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-08-27 11:18:38 +0800

    Update push().c

--- Level 3/01 - ADT Stack/push().c
+++ Level 3/01 - ADT Stack/push().c
@@ -1,1 +1,9 @@
-
+void push(Stack *S, char elem){
+    //check if full
+    if (S->top==MAX_SIZE-1){
+        printf("Full");
+    }
+    
+    S->top++; //increment for top pos
+    S->elem[S->top]=elem;
+}
