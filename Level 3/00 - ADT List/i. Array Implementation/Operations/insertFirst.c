b42641ff32e93e1c9d4d2c5b13fa049ca5575ab5
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-08-30 01:05:50 +0800

    Create insertFirst.c

---
+++ Level 3/00 - ADT List/i. Array Implementation/Operations/insertFirst.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX 10
+
+typedef struct
+{
+    char elem[MAX];
+    int count;
+} LIST;
+
+void insertFirst(LIST *A, char elem)
+{
+    if (A->count == MAX)
+    {
+        printf("Full");
+    }
+
+    for (int i = A->count; i > 0; i--)
+    {
+        A->elem[i] = A->elem[i - 1]; // shift elems to the right
+    }
+
+    A->elem[0] = elem; // at first index
+    A->count++;
+}
+
+int main()
+{
+    LIST A = {{'A', 'N', 'T', 'S'}, 4}; // init array
+
+    printf("Before: ");
+    for (int i = 0; i < A.count; i++)
+    {
+        printf("%c ", A.elem[i]); // A N T s
+    }
+    printf("\n");
+
+    insertFirst(&A, 'P');
+
+    printf("After inserting 'L' at pos 1: ");
+    for (int i = 0; i < A.count; i++)
+    {
+        printf("%c ", A.elem[i]); // P A N T S
+    }
+    printf("\n");
+
+    return 0;
+}
