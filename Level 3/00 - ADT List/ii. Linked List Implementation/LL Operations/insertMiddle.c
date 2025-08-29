f801b2c3d6de0a632e383b7959dfed443f807f1e
Cindy T. Lepatan <143421355+missglasses@users.noreply.github.com>
2025-08-30 01:24:07 +0800

    Update insertMiddle.c

--- Level 3/00 - ADT List/ii. Linked List Implementation/LL Operations/insertMiddle.c
+++ Level 3/00 - ADT List/ii. Linked List Implementation/LL Operations/insertMiddle.c
@@ -1,1 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+typedef struct node {
+    char data;
+    struct node *link;
+} cellType, *LIST;
+
+void insertMiddle(LIST *L, char elem) {
+    LIST newNode = (LIST)malloc(sizeof(cellType));
+    newNode->data = elem;
+    newNode->link = NULL;
+    
+    if (*L == NULL) {
+        *L = newNode;
+        return;
+    }
+    
+    int count = 0;
+    LIST temp = *L;
+    while (temp != NULL) {
+        count++;
+        temp = temp->link;
+    }
+    
+    int mid = count / 2;
+    
+    temp = *L;
+    for (int i = 0; i < mid - 1; i++) {
+        temp = temp->link;
+    }
+    
+    newNode->link = temp->link;
+    temp->link = newNode;
+}
+
+int main() {
+    LIST L = NULL;
+    
+    L = (LIST)malloc(sizeof(cellType));
+    L->data = 'M';
+    L->link = (LIST)malloc(sizeof(cellType));
+    L->link->data = 'P';
+    L->link->link = NULL;
+
+    printf("Before: ");
+    LIST temp = L;
+    while (temp != NULL) {
+        printf("%c ", temp->data);
+        temp = temp->link;
+    }
+    printf("\n");
+
+    insertMiddle(&L, 'A');
+
+    printf("After: ");
+    temp = L;
+    while (temp != NULL) {
+        printf("%c ", temp->data);
+        temp = temp->link;
+    }
+    printf("\n");
+
+    return 0;
+}
